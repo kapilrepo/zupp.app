a14a07680103d8dbfefd361fc2911e73227c8bb0
Kapil Sharma <techwonia@gmail.com>
2025-09-21 00:21:15 +0530

    Update port configuration to 80 Env new latest

---
+++ src/www/assets/login-Did2R1ZU.js
@@ -0,0 +1,2 @@
+import{j as e}from"./index-B5ACVTP-.js";const n=()=>e.jsxDEV("div",{children:"Login handled by root route"},void 0,!1,{fileName:"C:/waark/PALSORO/zupp.store/packages/admin/src/routes/login.tsx?tsr-split=component",lineNumber:2,columnNumber:30},void 0);export{n as component};
+//# sourceMappingURL=login-Did2R1ZU.js.map
